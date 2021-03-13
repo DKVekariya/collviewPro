137db0b562fac049c06f1d1a8ede69b4295b553e
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-03-13 16:42:05 +0530

    Correction done in code and storyboard

--- collviewPro/MainVC.swift
+++ collviewPro/MainVC.swift
@@ -10,11 +10,32 @@ import UIKit
 class MainVC: UIViewController {
 
     @IBOutlet weak var proCollectionView: UICollectionView!
+    var array = ["First Cell", "Second Cell", "Third Cell", "Fourth Cell", "Fifth Cell", "Sixth Cell"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        proCollectionView.dataSource = self
+        proCollectionView.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "proCell", for: indexPath) as! CollectionViewCell
+        cell.proLbl.text = array[indexPath.row]
+        return cell
+    }
+    
 
 
 }
+extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    
+}
+    
+
+
 
