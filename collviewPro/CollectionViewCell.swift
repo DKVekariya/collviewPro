168291500b818e1f2140047a7b5461efc3a44e21
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-03-14 10:46:01 +0530

    added collection view using programatic approach

--- collviewPro/CollectionViewCell.swift
+++ collviewPro/CollectionViewCell.swift
@@ -9,5 +9,27 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var proLbl: UILabel!
+    let proLbl = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        proLbl.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(proLbl)
+        
+        let centerY = proLbl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        centerY.isActive = true
+        let centerX = proLbl.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        centerX.isActive = true
+        
+        contentView.backgroundColor = .blue
+        proLbl.textColor = .white
+        proLbl.font = .systemFont(ofSize: 18)
+        
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- collviewPro/MainVC.swift
+++ collviewPro/MainVC.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class MainVC: UIViewController {
-
-    @IBOutlet weak var proCollectionView: UICollectionView!
-    var array = ["First Cell", "Second Cell", "Third Cell", "Fourth Cell", "Fifth Cell", "Sixth Cell"]
+    
+    lazy var proCollectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout() )
+        view.dataSource = self
+        view.delegate = self
+        view.register(CollectionViewCell.self, forCellWithReuseIdentifier: "proCell")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = nil
+        return view
+    }()
+    
+    var array = ["First Cell", "Second Cell", "Third Cell", "Fourth Cell", "Fifth Cell", "Sixth Cell", "Fifth Cell", "Sixth Cell"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        proCollectionView.dataSource = self
-        proCollectionView.delegate = self
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(proCollectionView)
+        
+        let array = [
+            proCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            proCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            proCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            proCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        array.forEach({ $0.isActive = true })
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: nil) { (_) in
+            self.proCollectionView.collectionViewLayout.invalidateLayout()
+        }
+    }
+}
+
+extension MainVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return array.count
     }
@@ -27,15 +54,26 @@ class MainVC: UIViewController {
         cell.proLbl.text = array[indexPath.row]
         return cell
     }
-    
-
-
-}
-extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
-
-    
 }
     
-
+extension MainVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let effectiveWith = collectionView.frame.width - self.collectionView(collectionView, layout: collectionViewLayout, minimumLineSpacingForSectionAt: indexPath.section) - 2*16
+        return CGSize.init(width: effectiveWith*0.5, height: effectiveWith*0.5)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        .init(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+}
 
 
